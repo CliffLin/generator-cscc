4a038e67a18322764ff8c3196a4f3dca24ae7393
CliffLin <cliff820119@gmail.com>
2015-08-05 16:38:33 +0800

    fix error
    1.package.json script error about path
    2.index.html common.js must before bundle.js
    3.webpack config error about path

--- generators/app/templates/webpack.config.js
+++ generators/app/templates/webpack.config.js
@@ -1,8 +1,10 @@
 var path = require("path");
-
+var webpack = require("webpack");
 module.exports = {
 	entry: {
-		bundle:['webpack/hot/dev-server',"./src/index.html","./src/boot.js"]
+		bundle:['webpack/hot/dev-server',
+				path.resolve(__dirname,"src/index.html"),
+				path.resolve(__dirname."src/boot.js")]
 	},
 	output: {
 		path: path.resolve(__dirname, '<%=folder %>'),
@@ -23,7 +25,11 @@ module.exports = {
 			{ test: /\.css$/, loader: "style!css"},
 			<% if (style==='saas') { %>{ test: /\.sass$/, loader: "style!css!sass" }, <% } %>
 			<% if (style==='less') { %>{ test: /\.less$/, loader: "style!css!less" }, <% } %>
-			{ test: /\.(html)$/, loader: "file?name=[path][name].[ext]&context=./src"}
+			{ test: /\.(html)$/, loader: "file?name=[path][name].[ext]&context=./client/src"}
 		],
 	},
+	plugins: [
+		new webpack.NoErrorsPlugin(),
+		new webpack.HotModuleReplacementPlugin()
+	]
 };
